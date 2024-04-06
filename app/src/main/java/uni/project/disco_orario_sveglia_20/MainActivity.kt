b0db871f3483488e44c2f331e7adde3233ef9882
beaby <beatricebastianello@gmail.com>
2024-04-06 19:13:27 +0200

    Initial commit

---
+++ app/src/main/java/uni/project/disco_orario_sveglia_20/MainActivity.kt
@@ -0,0 +1,11 @@
+package uni.project.disco_orario_sveglia_20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Disco_Orario_Sveglia_20"
+include(":app")
