0f3edcce0ba727c2b844a3a0deca1e4a233902df
beaby <beatricebastianello@gmail.com>
2024-04-07 00:14:15 +0200

    setting initial layout and components of the app

---
+++ app/src/main/java/uni/project/disco_orario_sveglia_20/HomeActivity.kt
@@ -0,0 +1,33 @@
+package uni.project.disco_orario_sveglia_20
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import uni.project.disco_orario_sveglia_20.databinding.ActivityHomeBinding
+import uni.project.disco_orario_sveglia_20.databinding.ActivityMainBinding
+
+class HomeActivity: AppCompatActivity(){
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(CountDown())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(CountDown())
+                R.id.myCar -> replaceFragment(MyCar())
+            }
+
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frameId,fragment).commit()
+    }
+}
--- app/src/main/java/uni/project/disco_orario_sveglia_20/MainActivity.kt
+++ app/src/main/java/uni/project/disco_orario_sveglia_20/MainActivity.kt
@@ -1,11 +1,18 @@
 package uni.project.disco_orario_sveglia_20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toHome = findViewById<Button>(R.id.autoTimeBt)
+        toHome.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
