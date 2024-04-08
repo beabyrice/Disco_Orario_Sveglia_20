89c7d423e340d6c30dbcc94e6e703b4a56b2cc3d
beaby <beatricebastianello@gmail.com>
2024-04-09 00:55:48 +0200

    first app style commits

--- app/src/main/java/uni/project/disco_orario_sveglia_20/MainActivity.kt
+++ app/src/main/java/uni/project/disco_orario_sveglia_20/MainActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val backgroundColor = ContextCompat.getColor(this, R.color.primaryColor)
+
+        // Set the background color programmatically
+
+
         val toHome = findViewById<Button>(R.id.autoTimeBt)
         toHome.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
